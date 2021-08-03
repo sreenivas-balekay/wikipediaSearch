96317a76bb859ba54a2fbeb4ab808df34af834ff
sreenivas <sreenivas569@gmail.com>
2021-08-03 10:19:12 +0530

    wikipedia clone

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();  
+  term = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit(){
+ 
+    console.log('this', this.term);
+    this.submitted.emit(this.term);
+  }
+
+}
